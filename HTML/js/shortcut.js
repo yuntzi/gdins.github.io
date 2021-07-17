1676d5c03957ac3a404e84c02b9f3ffbfdb4be0c
jygweb <jiangyaogeng@icloud.com>
2021-07-18 06:48:18 +0800

    Initial commit

---
+++ HTML/js/shortcut.js
@@ -0,0 +1,9 @@
+//id,iconName,iconUrl,url,width,height
+var shortcut = [
+[0,"每日一讲（1）","images/icos/logo_hd.png","https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzI1ODkxOTMzMQ==&action=getalbum&album_id=1415308106808082432&scene=173&from_msgid=2247484456&from_itemidx=1&count=3&uin=&key=&devicetype=Windows+10+x64&version=63010029&lang=zh_CN&ascene=1&session_us=gh_d4ebf84ce6c1&fontgear=2",900,540],
+[1,"海空军科院","images/icos/logo_hd.png","https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzI1ODkxOTMzMQ==&action=getalbum&album_id=1462584913450188804&scene=173&from_msgid=2247484456&from_itemidx=2&count=3#wechat_redirect",1000,500],
+[2,"豆瓣FM","images/icos/news.png","http://douban.fm/",1024,500],
+[3,"优酷","images/icos/youku.png","http://www.youku.com/",1000,500],
+[4,"迷宫游戏","images/icos/youku.png","game1.html",1000,500],
+[5,"问题解决","images/icos/son.png","xxx.html",1000,500],
+];
